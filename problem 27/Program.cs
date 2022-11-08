7b28a7ac387165bc4e72ab877f13c8f97ca21b09
Tamerlan <dzhangtim@list.ru>
2022-11-08 18:44:42 +0300

    newfile3

---
+++ problem 27/Program.cs
@@ -0,0 +1,12 @@
+﻿int count = int.Parse(Console.ReadLine());
+Console.WriteLine(GetSum(count));
+
+int GetSum(int count){
+    int sum = 0;
+    while(true){
+        if(count == 0) break;
+        count = count/10;
+        sum = sum + count/10;
+    }
+    return sum;
+}
